5937e6af64dd2d17e3950ae2652df23cb70ae731
Baldean Stefan-Tudor <baldeanstefan420@gmail.com>
2026-03-29 17:01:17 +0300

    Updated logic

--- TeamNut/Services/MealService.cs
+++ TeamNut/Services/MealService.cs
@@ -1,8 +1,48 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using NutApp.Domain;
+using NutApp.Backend.Repositories;
 
-public class Class1
+namespace NutApp.Backend.Services
 {
-	public Class1()
-	{
-	}
+    public class MealService
+    {
+        private readonly MealRepository _mealRepository;
+
+        
+        public MealService()
+        {
+            _mealRepository = new MealRepository();
+        }
+
+        
+        public List<Meal> GetMeals(string filter = null)
+        {
+            
+            if (string.IsNullOrWhiteSpace(filter) || filter == "All")
+            {
+                return _mealRepository.GetAllMeals();
+            }
+
+            
+            return _mealRepository.GetMealsByFilter(filter);
+        }
+
+        
+        public void ToggleFavorite(int userId, int mealId, bool currentlyFavorited)
+        {
+            if (currentlyFavorited)
+            {
+                _mealRepository.RemoveFavorite(userId, mealId);
+            }
+            else
+            {
+                _mealRepository.AddFavorite(userId, mealId);
+            }
+        }
+
+        public List<Meal> GetUserFavorites(int userId)
+        {
+            return _mealRepository.GetUserFavorites(userId);
+        }
+    }
 }
