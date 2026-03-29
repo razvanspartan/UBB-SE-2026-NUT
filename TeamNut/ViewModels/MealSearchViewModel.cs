9cddf2886a7a3f22e57661be03d3f675e39662d8
Guilherme Chagas <gcsnomerciy@gmail.com>
2026-03-30 00:39:38 +0300

    refactor: removed mock data from MealSearchViewModel

--- TeamNut/ViewModels/MealSearchViewModel.cs
+++ TeamNut/ViewModels/MealSearchViewModel.cs
@@ -1,31 +1,18 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 using TeamNut.Models;
 
 namespace TeamNut.ViewModels
 {
     public class MealSearchViewModel
     {
-        private List<Meal> meals;
         public MealSearchViewModel()
         {
-            meals = new List<Meal>
-    {
-        new Meal { Name = "Chicken Rice", Calories = 500 },
-        new Meal { Name = "Beef Burger", Calories = 700 },
-        new Meal { Name = "Salad", Calories = 200 },
-        new Meal { Name = "Pasta", Calories = 600 }
-    };
+            // Ready for future service integration
         }
 
         public List<Meal> SearchMeals(MealFilter filter)
         {
-            if (filter == null || string.IsNullOrEmpty(filter.SearchTerm))
-                return meals;
-
-            return meals
-                .Where(m => m.Name.ToLower().Contains(filter.SearchTerm.ToLower()))
-                .ToList();
+            return new List<Meal>(); // vnothing until
         }
 
         public void ToggleFavorite(Meal meal)
