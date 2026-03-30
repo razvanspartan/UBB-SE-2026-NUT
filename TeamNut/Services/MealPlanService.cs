02aaf7a861820553230d56498d67185fc7877d6d
Baldean Stefan-Tudor <baldeanstefan420@gmail.com>
2026-03-30 16:22:52 +0300

    Implemented Repo, Service and Model for meal plans

---
+++ TeamNut/Services/MealPlanService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TeamNut.Models;
+using TeamNut.Services;
+using TeamNut.Repositories;
+
+namespace TeamNut.Services
+{
+    public class MealPlanService
+    {
+        private readonly MealPlanRepository _mealPlanRepository;
+        
+        
+
+        public MealPlanService()
+        {
+            _mealPlanRepository = new MealPlanRepository();
+            
+        }
+
+        public async Task GenerateDailyPlan(int userId, string goal, List<int> selectedMealIds)
+        {
+            
+            var newPlan = new MealPlan
+            {
+                UserId = userId,
+                CreatedAt = DateTime.Now,
+                GoalType = goal
+            };
+
+            await _mealPlanRepository.Add(newPlan);
+        }
+
+        public async Task<IEnumerable<MealPlan>> GetHistory(int userId)
+        {
+            
+            return await _mealPlanRepository.GetAll();
+        }
+    }
+}
