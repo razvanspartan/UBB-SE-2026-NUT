390f32c704e34caafd092e8cdac82f7069e59e42
Ionut Burz <burz.ionut62@yahoo.com>
2026-03-30 04:51:35 +0300

    [NUT-77] - Create Meal Plan ViewModel

--- TeamNut/Views/MealPlanView/MealPlanPage.xaml.cs
+++ TeamNut/Views/MealPlanView/MealPlanPage.xaml.cs
@@ -1,11 +1,10 @@
 ﻿using Microsoft.UI.Xaml.Controls;
-using TeamNut.ModelViews; // English: Ensure this points to your ModelViews folder
+using TeamNut.ModelViews;
 
 namespace TeamNut.Views.MealPlanView
 {
     public sealed partial class MealPlanPage : Page
     {
-        // CRITICAL: This MUST be public for x:Bind to work!
         public MealPlanViewModel ViewModel { get; } = new MealPlanViewModel();
 
         public MealPlanPage()
