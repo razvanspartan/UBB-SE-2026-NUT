b98a6b910c8288b4680ad23a9aeee46f48e87ba9
Guilherme Chagas <gcsnomerciy@gmail.com>
2026-03-31 23:18:42 +0300

    Fix syntax error in MealSearchViewModel

--- TeamNut/ViewModels/MealSearchViewModel.cs
+++ TeamNut/ViewModels/MealSearchViewModel.cs
@@ -1,7 +1,5 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 using TeamNut.Models;
-using TeamNut.Repositories;
 
 namespace TeamNut.ViewModels
 {
@@ -11,47 +9,12 @@ namespace TeamNut.ViewModels
 
         public MealSearchViewModel()
         {
-            try
-            {
-                var repo = new MealRepository();
-                meals = repo.GetMeals(); // DATABASE CALL
-            }
-            catch
-            {
-                // check crash 
-                meals = new List<Meal>();
-            }
+            meals = new List<Meal>();
         }
 
         public List<Meal> SearchMeals(MealFilter filter)
         {
-            var result = meals;
-
-            // 🔍 SEARCH
-            if (!string.IsNullOrWhiteSpace(filter.SearchTerm))
-            {
-                result = result
-                    .Where(m => m.Name.ToLower().Contains(filter.SearchTerm.ToLower()))
-                    .ToList();
-            }
-
-            // 🔥 FILTERS
-            if (filter.IsVegan)
-                result = result.Where(m => m.IsVegan).ToList();
-
-            if (filter.IsKeto)
-                result = result.Where(m => m.IsKeto).ToList();
-
-            if (filter.IsGlutenFree)
-                result = result.Where(m => m.IsGlutenFree).ToList();
-
-            if (filter.IsLactoseFree)
-                result = result.Where(m => m.IsLactoseFree).ToList();
-
-            if (filter.IsNutFree)
-                result = result.Where(m => m.IsNutFree).ToList();
-
-            return result;
+            return meals;
         }
 
         public void ToggleFavorite(Meal meal)
