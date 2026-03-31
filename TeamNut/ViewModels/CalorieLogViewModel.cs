fce89554a2e2e569953438b64b89382c9d5da358
0xClaut <claudiu.grigore@stud.ubbcluj.ro>
2026-03-31 20:06:42 +0300

    view and viewmodel added, not working( problems with InitializeComponent();)

---
+++ TeamNut/ViewModels/CalorieLogViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using TeamNut.Models;
+using TeamNut.Services;
+
+namespace TeamNut.ViewModels
+{
+    public partial class CalorieLoggingViewModel : ObservableObject
+    {
+        private readonly CalorieLogService _service;
+
+        public CalorieLoggingViewModel()
+        {
+            _service = new CalorieLogService();
+        }
+
+        [ObservableProperty]
+        private bool _hasData;
+
+        [ObservableProperty]
+        private string _statusMessage;
+
+        [ObservableProperty]
+        private CalorieLog _dailyLog = new();
+
+        [ObservableProperty]
+        private CalorieLog _weeklyLog = new();
+
+        public async Task Load(DateTime mealPlanDate)
+        {
+            if (!_service.HasDayPassed(mealPlanDate))
+            {
+                HasData = false;
+                StatusMessage = "Not enough information to display";
+                return;
+            }
+
+            HasData = true;
+
+            DailyLog = await _service.GetDailyLog();
+            WeeklyLog = await _service.GetWeeklyTotals();
+        }
+    }
+}
---
+++ TeamNut/Views/CalorieLoggingView/CalorieLoggingPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.UI.Xaml.Controls;
+using TeamNut.ViewModels;
+
+namespace TeamNut.View
+{
+    public sealed partial class CalorieLoggingPage : Page
+    {
+        private readonly CalorieLoggingViewModel _viewModel;
+
+        public CalorieLoggingPage()
+        {
+            this.InitializeComponent();
+
+            _viewModel = new CalorieLoggingViewModel();
+            this.DataContext = _viewModel;
+
+            LoadData();
+        }
+
+        private async void LoadData()
+        {
+            await _viewModel.Load(System.DateTime.Now.AddDays(-1));
+        }
+    }
+}
