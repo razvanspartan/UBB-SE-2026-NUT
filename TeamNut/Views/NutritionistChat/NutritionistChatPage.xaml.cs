2960b7dfd0f56bf695f5e043fee9356ad4516a57
anisiaaa <bantuanisia@gmail.com>
2026-03-31 03:05:39 +0100

    nutritionist chat view added

---
+++ TeamNut/Views/NutritionistChat/NutritionistChatPage.xaml.cs
@@ -0,0 +1,12 @@
+using Microsoft.UI.Xaml.Controls;
+
+namespace TeamNut.Views.NutritionistChat
+{
+    public sealed partial class NutritionistChatPage : Page
+    {
+        public NutritionistChatPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
